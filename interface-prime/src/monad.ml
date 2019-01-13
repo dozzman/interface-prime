8126d3d9951d39ebe7ebabc6ccc685f7be8c73a6
Dorian <dorian@kudi.ai>
2019-01-13 23:25:13 +0000

    Remove commented out code

--- interface-prime/src/monad.ml
+++ interface-prime/src/monad.ml
@@ -27,25 +27,4 @@ end
 module type ErrorType = sig
   type t
   val of_exn : exn -> t
-(*  val of_string : string -> t *)
 end
-
-(*module Make_result(E : ErrorType)(M : S) = struct
-  let fail x = Error x |> M.return
-  let lift m = M.bind m (fun x -> Ok x |> M.return)
-  let lift_opt e m = M.bind m (function
-    | Some x -> Ok x |> M.return
-    | None -> e |> M.return)
-  include Make(struct
-    type +'a t = ('a, E.t) result M.t
-    let return x = Ok x |> M.return
-    let bind m f = M.bind m (function
-      | Ok x ->
-        (try
-          f x
-        with e ->
-          e |> E.of_exn |> fail)
-      | Error x -> fail x)
-  end)
-end
-*)
